308f0ac93bf055a71296f1a35d62b7e73976f3cc
Pavel Lipatov <palipatov@gmail.com>
2019-03-10 18:07:10 +0300

    WIP.

--- server/app.py
+++ server/app.py
@@ -1,13 +1,20 @@
 from flask import Flask
-from flask-restful import Resource, Api
+from flask_restful import Resource, Api
 import logging
+# from server import db
 import db
 
 logger = logging.getLogger()
+handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s-%(levelname)-8s %(message)s')
-logger.setFormatter(formatter)
+handler.setFormatter(formatter)
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
+
 
 logger.setLevel(logging.INFO)
 
 base = db.ManageTodoDB("db.sqlite")
 base.create_db_struct()
+base.add_user("Michael")
+base.add_task("Test 1", "This my first task", 1, "2019-03-10", "2019-03-11")
--- server/db.py
+++ server/db.py
@@ -17,7 +17,7 @@ class ManageTodoDB:
             self.db = sqlite3.connect(self.db_file)
             logger.info("Connected to DB: %s", self.db_file)
         except sqlite3.Error as e:
-            logger.error('Error connecting to DB: %s', e.message)
+            logger.error('Error connecting to DB: %s', e)
 
     def close_db(self):
         self.db.close()
@@ -28,9 +28,9 @@ class ManageTodoDB:
                 task_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 task_name VARCHAR(250) NOT NULL,
                 task_description VARCHAR(1000) NOT NULL,
-                task_assignee INT NOT NULL,
-                task_start_data DATE, task_end_date DATE,
-                FOREIGN KEY (task_assignee) REFERENCES (user_id)
+                task_assignee INTEGER NOT NULL,
+                task_start_date DATE, task_end_date DATE,
+                FOREIGN KEY (task_assignee) REFERENCES users(user_id)
             );
                  
             CREATE TABLE IF NOT EXISTS users (
@@ -38,9 +38,44 @@ class ManageTodoDB:
                 user_name VARCHAR(50) NOT NULL
             );
               '''
-
+        c = self.db.cursor()
         try:
-            self.db.execute(sql)
+            c.executescript(sql)
+            logger.info("DB structure was created!")
         except sqlite3.Error as e:
-            logger.error("Error creating DB: %s", e.message)
+            logger.error("Error creating DB: %s", e)
             return e
+
+    def add_user(self, username):
+        sql = '''
+            INSERT INTO users (user_name) VALUES("{:s}")    
+        '''.format(username)
+        c = self.db.cursor()
+        try:
+            c.executescript(sql)
+            logger.info("Successfully added user %s", username)
+        except sqlite3.Error as e:
+            logger.error("Error adding user %s. Message: %s", username, e)
+            return e
+
+    def add_task(self, name, description, assignee, start_date, end_date):
+        sql = '''
+            INSERT INTO tasks (task_name, task_description, task_assignee, task_start_date, task_end_date)
+            VALUES("{:s}", "{:s}", {:d}, "{:s}", "{:s}")
+        '''.format(name, description, assignee, start_date, end_date)
+
+        c = self.db.cursor()
+        try:
+            c.executescript(sql)
+            logger.info("Successfully added task: %s", name)
+        except sqlite3.IntegrityError as e:
+            logger.error("DB integrity error: %s", e)
+            return e
+
+    def remove_db(self):
+        sql = '''
+            DROP DATABASE;
+        '''
+        c = self.db.cursor()
+
+        return c.executescript(sql)
