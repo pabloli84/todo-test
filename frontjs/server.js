592a50fdd6ef32a254060c9b45b566a01dddd492
Lipatova <Raisa.Lipatova@billing.ru>
2019-03-21 00:26:46 +0300

    Css fixed. Added get request

--- frontjs/server.js
+++ frontjs/server.js
@@ -4,6 +4,8 @@ const request = require('request');
 const app = express();
 
 app.use(express.static('public'));
+//app.use(express.static(__dirname + '/css'));
+app.use('/css',express.static(__dirname +'/css'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
@@ -21,7 +23,7 @@ app.post('/createTask', function (req, res) {
   let taskAssignee = req.body.taskAssignee;
   let taskStartDate = req.body.taskStartDate;
   let taskEndDate = req.body.taskEndDate;
-  
+
   // JSON to be passed to the QPX Express API
     var requestData = {
     "task_name": taskName,
@@ -39,6 +41,35 @@ app.post('/createTask', function (req, res) {
     }, function (error, response, responseBody) {
        if (!error && response.statusCode === 201) {
             console.log(responseBody)
+             //res.render('index', {  srt: 'sdfsdfsd' });
+             res.statusCode = 201;
+             res.setHeader('Content-Type', 'application/json');
+             res.responseBody = responseBody;
+             res.end(JSON.stringify(responseBody));
+        }
+        else {
+            res.statusCode = response.statusCode;
+            res.responseBody = responseBody.responseBody;
+            console.log("error: " + error)
+            console.log("response.statusCode: " + response.statusCode)
+            console.log("response.statusText: " + response.statusText)
+        }
+    
+  });
+})
+
+
+app.get('/getTasks', function (req, res) {
+   
+  let url = `https://damp-eyrie-62274.herokuapp.com/tasks`
+
+  request({
+    url: url,
+    method: "GET"
+    }, function (error, response, responseBody) {
+       if (!error && response.statusCode === 200) {
+            console.log(responseBody)
+            // res.render('index', {  srt: 'sdfsdfsd' });
         }
         else {
 
@@ -46,7 +77,7 @@ app.post('/createTask', function (req, res) {
             console.log("response.statusCode: " + response.statusCode)
             console.log("response.statusText: " + response.statusText)
         }
-    res.statusCode = 201;
+    res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.responseBody = responseBody;
     res.end(JSON.stringify(responseBody));
